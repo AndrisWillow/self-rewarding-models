9d23f3aa1798e3a3d39b6d3dd156627788ab6649
AndrisWillow <andris.vitols@gmail.com>
2024-05-08 13:39:21 +0300

    Added IFEval results, updated arc-chalange

--- app/benchmarks/arc-chalange/arc-chalange.py
+++ app/benchmarks/arc-chalange/arc-chalange.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 import torch
-from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer, DataCollatorWithPadding
+from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer
 import os
 from peft import PeftModel
 import re
@@ -24,11 +24,8 @@ def load_model_with_adapter(base_model, adapter_path):
 def format_prompt(ds_row):
     question = ds_row['question']
     choices_formatted = " ".join([f"{label}: {text}" for label, text in zip(ds_row['choices']['label'], ds_row['choices']['text'])])
-#     prompt = f'''Answer this multiple choice question. 
-# Question: {question} Possible answers: {choices_formatted}. Output the answer in this format <Answer>x</Answer> where x is is the corresponding letter to the correct answer. 
-# <Answer>'''
     prompt = f'''Answer this multiple choice question. 
-Question: {question} Possible answers: {choices_formatted}. Output only the corresponding letter to the correct answer. Answer: ''' 
+Question: {question} Possible answers: {choices_formatted}. Output only the corresponding letter or number to the correct answer. Answer: ''' 
     return prompt
 
 def model_generate_output_batched(model, tokenizer, prompts):
@@ -38,57 +35,53 @@ def model_generate_output_batched(model, tokenizer, prompts):
     tokens = model_inputs["input_ids"].to(model.device)
     attention_mask = model_inputs["attention_mask"].to(model.device)
 
-    print("tokens shape:", tokens.shape)  # Debug print
-    print("attention_mask shape:", attention_mask.shape)  # Debug print
+    # print("tokens shape:", tokens.shape)  # Debug print
+    # print("attention_mask shape:", attention_mask.shape)  # Debug print
 
-    print("Tokens before padding:")
-    for idx, token_ids in enumerate(tokens):
-        print(f"Input {idx + 1} - Input IDs:", token_ids)
-        print(f"Input {idx + 1} - Attention Mask:", attention_mask[idx])
-        print("Decoded input:", tokenizer.decode(token_ids, skip_special_tokens=True))
-        print()  # Add an empty line for readability
+    # print("Tokens before padding:")
+    # for idx, token_ids in enumerate(tokens):
+    #     print(f"Input {idx + 1} - Input IDs:", token_ids)
+    #     print(f"Input {idx + 1} - Attention Mask:", attention_mask[idx])
+    #     print("Decoded input:", tokenizer.decode(token_ids, skip_special_tokens=True))
+    #     print("\n")
 
     generation_output = model.generate(
         tokens,
-        max_new_tokens=1,
+        max_new_tokens=3, # Genearting a few tokens, because there can be noise like new lines etc.
         pad_token_id=tokenizer.pad_token_id,
     )
 
-    print("Generated tokens:")
-    for token_ids in generation_output:
-        print(tokenizer.decode(token_ids, skip_special_tokens=True))
+    # print("Generated tokens:")
+    # for token_ids in generation_output:
+    #     print(tokenizer.decode(token_ids, skip_special_tokens=True))
     output = tokenizer.batch_decode(generation_output[:, tokens.shape[1]:], skip_special_tokens=True)
-    print("Output")
-    print(output)
+    # print("Output")
+    # print(output)
     return output
 
-
-def eval_benchmark(model, tokenizer, dataset, model_name_or_path, adapter="None", batch_size=10):
-    score = 0
+# Evaluating the benchmark in a loose way by extracting answers, because the model can be noisy and may output additional things not just an answer
+def eval_benchmark_save_results(model, tokenizer, dataset, model_name_or_path, adapter="None", batch_size=1):
+    score = failed_generations = 0
     total_rows_to_evaluate = len(dataset)
-    failed_generations = 0
+    answer_regex = re.compile(r"[ABCD1234]") #posible answers, taken from arc dataset
 
     for idx in range(0, total_rows_to_evaluate, batch_size):
-        batch_end = min(idx + batch_size, total_rows_to_evaluate) # Avoid overflow
+        batch_end = min(idx + batch_size, total_rows_to_evaluate)
+
         prompts = [format_prompt(dataset[row]) for row in range(idx, batch_end)]
         answer_keys = [dataset[row]['answerKey'] for row in range(idx, batch_end)]
 
         outputs = model_generate_output_batched(model, tokenizer, prompts)
-
         for output, answer_key in zip(outputs, answer_keys):
-            print(output, answer_key)
-            # answer_regex = re.compile(r"(.*?)</Answer>")
-            # match = answer_regex.search(output)
-            # print(output, answer_key)
-            # if match and match.group(1) == answer_key:
-            #     score += 1
-            if output == answer_key: 
-                score += 1 
+            match = answer_regex.search(output)
+            if match:
+                if match.group() == answer_key:
+                    score += 1
             else:
-                failed_generations += 1 # TODO mark it as a failed generation if output is not one of the possible answers (A,B,C,D,1,2,3,4)
-        print(f"Processed batch ending at index {batch_end}, current score: {score}")
+                failed_generations += 1
 
-    # Calculating result and writing to JSON
+        print(f"Processed batch ending at index {batch_end}, current score: {score}, failed generations: {failed_generations}")
+
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     output_file = f"benchmark_results{timestamp}.json" 
     result = score / total_rows_to_evaluate
@@ -107,16 +100,17 @@ def eval_benchmark(model, tokenizer, dataset, model_name_or_path, adapter="None"
 
 def main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    adapter_path = "None" # TODO add shell params
-    # adapter_path = os.path.join(script_dir, '../../../outputs/Mistral-7B-Instruct-v0.2-SFT_baseline-DPO-M1')
-    model_name_or_path = "mistralai/Mistral-7B-Instruct-v0.2"
+    adapter_name = "None" # TODO add shell params
     dataset = load_dataset('ai2_arc', 'ARC-Challenge', split="test")
 
+    model_name_or_path = "mistralai/Mistral-7B-Instruct-v0.2"
     model, tokenizer = get_model_and_tokenizer(model_name_or_path)
+
+    # adapter_name = 'Mistral-7B-Instruct-v0.2-SFT_baseline-DPO-M1'
+    # adapter_path = os.path.join(script_dir, f'../../../outputs/{adapter_name}')
     # model = load_model_with_adapter(model, adapter_path)
     model.eval()
-
-    eval_benchmark(model, tokenizer, dataset, model_name_or_path, adapter_path, batch_size=5)
+    eval_benchmark_save_results(model, tokenizer, dataset, model_name_or_path, adapter_name, batch_size=1)
 
 if __name__ == "__main__":
     main()
